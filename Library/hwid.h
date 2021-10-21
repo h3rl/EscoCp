d371c4809aba27aacd4c796f06caf69ab7682899
h3rl <halvard.erla@gmail.com>
2021-10-21 14:41:33 +0200

    optimized identify grabbing and made it easier to understand

--- Library/hwid.h
+++ Library/hwid.h
@@ -1,1 +1,23 @@
 #pragma once
+#include "md5.h"
+#include "wmi/wmi.hpp"
+#include "wmi/wmiclasses.hpp"
+#include "debug.h"
+
+#include <intrin.h> //__cpuid
+#include <Windows.h>
+#include <winuser.h>
+#include <iostream>
+using namespace std;
+using namespace Wmi;
+
+extern "C" __declspec(dllexport) const char* getIdentifier();
+
+#define __abs(x) (x < 0 ? -(x) : x)
+
+string getDriveSerial(const char* drive);
+string getGpuid();
+string getBoardid();
+string getComputerName();
+string getNetworkid();
+string getCpuHash();
