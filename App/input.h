a92921e68ed4ea73ab2255ee3b7ac7a5acfd40cc
Halvard Erlandsen <267227@uis.no>
2022-02-05 01:19:57 +0100

    add setmousepos and lbuttondown and up

--- App/input.h
+++ App/input.h
@@ -1,6 +1,10 @@
 #pragma once
 namespace input {
 	void move(short x, short y);
+	void SetMousePos(int x, int y);
 	void click(short delay);
 	void pressKey(int vkey, int delay);
+
+	void MouseButtonLDown();
+	void MouseButtonLUp();
 }
